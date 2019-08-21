fd2989f644426e46815125809acab1cc4d051d81
Alin Stefan Olaru <alin.stefan.olaru@lego.com>
2019-08-21 13:55:55 +0200

    Added giphy top 10 image thing

--- planningpoker.user.js
+++ planningpoker.user.js
@@ -6,27 +6,43 @@
 // @grant           none
 // @downloadURL     https://alinstefanola.ru/Custom-Planning-Poker/planningpoker.user.js
 // @updateURL       https://alinstefanola.ru/Custom-Planning-Poker/planningpoker.user.js
-// @version         0.02
+// @version         0.03
 // @run-at          document-end
 // ==/UserScript==
 
 var CustomPoker = {
+  getRandomImg: keyword => {
+    $.get(
+      `http://api.giphy.com/v1/gifs/search?api_key=VEPCxEQD0VbuDuJ2eZZwXW6ztR6VdxWr&q=${keyword}&limit=10`,
+      res => {
+        for(var i = 0; i < 10; i++) {
+          $(`#fancy-image-${i}`).attr('src', res.data[i].images.original.url);
+          $(`#fancy-image-${i}`).click(() => {
+            $("#custom-card-input").val(res.data[i].images.original.url);
+          })
+        }
+      }
+    );
+  },
   initPreviewCard: () => {
-    var cardImage = $("<img>").attr({
-      id: "fancy-image",
-      src: "https://media.giphy.com/media/AXQaLoWMeSmRy/giphy.gif"
-    });
-    var cardPreview = $("<div>")
-      .attr({ class: "SelectableCard col-md-1" })
-      .css({
-        display: "block"
-      })
-      .append(cardImage);
+    for(var i = 0; i < 10; i++) {
+      var cardImage = $("<img>").attr({
+        id: `fancy-image-${i}`,
+        src: "https://media.giphy.com/media/AXQaLoWMeSmRy/giphy.gif"
+      });
+      var cardPreview = $("<div>")
+        .attr({ class: "SelectableCard col-md-1" })
+        .css({
+          display: "block"
+        })
+        .append(cardImage);
+  
+      $("#ControlPanel")
+        .find('img[src$="/Content/Cards/CardBreak.png"]')
+        .parent()
+        .after(cardPreview);
 
-    $("#ControlPanel")
-      .find('img[src$="/Content/Cards/CardBreak.png"]')
-      .parent()
-      .after(cardPreview);
+    }
   },
   initButton: () => {
     var btn = $("<button>")
@@ -39,6 +55,14 @@ var CustomPoker = {
         hub.server.selectCard(text);
       });
     $("#ControlPanel").append(btn);
+    var btnTwo = $("<button>").attr({
+      'class': "btn-btn-default"
+    }).text("Lookup on Giphy").click(() => {
+      var text = $("#custom-card-input").val();
+
+      CustomPoker.getRandomImg(text);
+    })
+    $('#ControlPanel').append(btnTwo);
   },
   initInput: () => {
     var input = $("<input>")
